e9e80fd99a4eb80c096eae5921caa3c816c28b16
magnoou <mignime.dev@gmail.com>
2018-12-10 21:21:58 -0600

    Setting api url in environment variable

--- src/app/community/services/community.service.ts
+++ src/app/community/services/community.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +17,7 @@ export class CommunityService {
   private members: Member[] = [];
   private membersUpdated = new Subject<{ members: Member[] }>();
   
-  private url = 'http://22HW020536:8080/';
-  private localUrl = 'http://10.101.170.1:8080/';
-  private homeUrl = 'http://192.168.100.130:8080/';
+  private url = environment.apiUrl;
 
   // private communityTypes: CommunityType[] = [{ community_type_id: 1, name: 'Community type 1' }, { community_type_id: 2, name: 'Community type 2' }];
 
@@ -35,7 +34,7 @@ export class CommunityService {
 
 
   getGovernanceLevel() {
-    return this.http.get(this.homeUrl + 'governance/level');
+    return this.http.get(this.url + 'governance/level');
   }
 
   setCommunityAttributes(communityTipe: CommunityType, name: string, description: string) {
