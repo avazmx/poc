6fea6365127a93fef410f226302e55da78949552
Hector Franco <hectorft.hf@gmail.com>
2018-12-16 00:55:38 -0600

    Services integration and fill first tab object

--- src/app/community/models/geo-services.model.ts
+++ src/app/community/models/geo-services.model.ts
@@ -1,8 +1,13 @@
+import { Country } from 'src/app/shared/models/country.model';
+import { District } from 'src/app/shared/models/district.model';
 import { State } from 'src/app/shared/models/state.model';
+
 import { BussinesUnit } from './bussines-unit.model';
 
 export class GeoService {
     public id: number;
+    public country: Country;
+    public district: District;
     public state: State;
     public slicRangeLow: number;
     public slicRangeHigh: number;
