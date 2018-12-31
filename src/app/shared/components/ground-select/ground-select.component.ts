4fd01df6d13f481515b0af7c68776e8016c98eb6
magnoou <mignime.dev@gmail.com>
2018-12-31 14:29:12 -0600

    Save checks values on back button

--- src/app/shared/components/ground-select/ground-select.component.ts
+++ src/app/shared/components/ground-select/ground-select.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { Store } from '@ngrx/store';
+import { Community } from 'src/app/community/models/community.model';
 
 @Component({
   selector: 'ups-ground-select',
@@ -11,10 +13,12 @@ export class GroundSelectComponent implements ICellRendererAngularComp {
   public params: any;
   public cell: any;
   public groundChecked = false;
+  public communityObject: Community;
+  public isShow = false;
 
   public currentRow: number;
 
-  constructor() { }
+  constructor(private store: Store<Community>) { }
 
   // AG Grid Initialize
   agInit(params: any) {
@@ -23,6 +27,11 @@ export class GroundSelectComponent implements ICellRendererAngularComp {
     this.cell = { row: params.value, col: params.colDef.headerName };
 
     this.currentRow = +this.params.node.id;
+    this.store.select('community').subscribe((obj: Community) => {
+      if (obj.activeTab === 1 && obj.geoServices && obj.geoServices[this.currentRow]) {
+        this.groundChecked = obj.geoServices[this.currentRow].ground;
+      }
+    });
   }
 
   // AG Grid reload
